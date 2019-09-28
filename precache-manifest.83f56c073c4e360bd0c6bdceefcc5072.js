ac9638a312768196f6671d94b4a1551b134241e3
Kyle Copeland <copelandky@gmail.com>
2019-09-27 21:36:46 -0700

    Updates

---
+++ precache-manifest.83f56c073c4e360bd0c6bdceefcc5072.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "221e23607d338426d67b6e635ee69baf",
+    "url": "/20-for-20s/index.html"
+  },
+  {
+    "revision": "9c2fb2971e02f3e52098",
+    "url": "/20-for-20s/static/css/main.1724fa97.chunk.css"
+  },
+  {
+    "revision": "499400754a0346f2b1e9",
+    "url": "/20-for-20s/static/js/2.744b6df2.chunk.js"
+  },
+  {
+    "revision": "9c2fb2971e02f3e52098",
+    "url": "/20-for-20s/static/js/main.6c4e68de.chunk.js"
+  },
+  {
+    "revision": "bc0dfc6a04f33e1d9fc3",
+    "url": "/20-for-20s/static/js/runtime-main.b28d1869.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/20-for-20s/static/media/logo.25bf045c.svg"
+  }
+]);
