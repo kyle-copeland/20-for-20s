cbd1c5f314ab2fb53533824046535823b93ed75a
Kyle Copeland <copelandky@gmail.com>
2019-09-27 21:04:07 -0700

    feat: add build for github pages

---
+++ build/precache-manifest.34bb9c7515d6cca89e37603dfefb4c56.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eb92b1031a6a211d3461825bcec159b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c35c4bb12b099e596c6a",
+    "url": "/static/css/main.1724fa97.chunk.css"
+  },
+  {
+    "revision": "499400754a0346f2b1e9",
+    "url": "/static/js/2.744b6df2.chunk.js"
+  },
+  {
+    "revision": "c35c4bb12b099e596c6a",
+    "url": "/static/js/main.b9c50f32.chunk.js"
+  },
+  {
+    "revision": "9ee29293be53102c8572",
+    "url": "/static/js/runtime-main.79ffb492.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
